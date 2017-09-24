8dd94bd91f2558a02e1e5d08acb2a05686a4ee82
xdiviatorx <xdiviatorx@mail.ru>
2017-09-24 22:32:19 +0300

    binding

--- app/src/main/java/com/technologies/mobile/testapp/android/activities/DetailedNewsActivity.java
+++ app/src/main/java/com/technologies/mobile/testapp/android/activities/DetailedNewsActivity.java
@@ -1,15 +1,19 @@
 package com.technologies.mobile.testapp.android.activities;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.technologies.mobile.testapp.R;
+import com.technologies.mobile.testapp.databinding.ActivityDetailedNewsBinding;
 
 public class DetailedNewsActivity extends AppCompatActivity {
+
+    ActivityDetailedNewsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailed_news);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detailed_news);
     }
 }
