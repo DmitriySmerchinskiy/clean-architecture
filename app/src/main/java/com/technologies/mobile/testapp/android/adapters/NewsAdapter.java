99211ae2aed1f7b5aa15d22b49e499c6d8b7fa31
xdiviatorx <xdiviatorx@mail.ru>
2017-09-24 22:20:38 +0300

    news adapter

---
+++ app/src/main/java/com/technologies/mobile/testapp/android/adapters/NewsAdapter.java
@@ -0,0 +1,10 @@
+package com.technologies.mobile.testapp.android.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by diviator on 24.09.2017.
+ */
+
+public class NewsAdapter extends RecyclerView.Adapter implements INewsAdapter {
+}
